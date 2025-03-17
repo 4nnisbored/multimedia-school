55c54fbb51a9c8e494c1ed100808d30525eedd0d
guyisbored <niod351@gmail.com>
2025-03-17 12:32:37 -0400

    update

---
+++ public_html/web2/project1/project1.js
@@ -0,0 +1,27 @@
+const myContent = document.getElementById("desc")
+        function roadkill() {
+            myContent.innerHTML = `<figure><img src="https://f4.bcbits.com/img/a3240128293_10.jpg"> <h2>What to Do When the Earth Swallows You Whole</h2> <h3>roadkill</h3></figure> <p>truth be told i know very little about roadkill. they're a small act out of Lisbon, Portugal and haven't made much music to date, but their debut album shows they are an artist to look out for. This alternitive indie folk album comes with a sombre, almost depressing atmosphere, but mostly seems to talk about different theoretical ends of the world and ways they spent it.</p>`
+        }
+        function chainsaw() {
+            myContent.innerHTML = '<img src="https://f4.bcbits.com/img/a0001639035_10.jpg" <h2>Chainsaw Girl</h2> <h3>Chainsaw Girl</h3> <p></p>'
+        }
+
+        function goodgod() {
+            myContent.innerHTML
+        }
+
+        function fishmonger() {
+            myContent.innerHTML = `<figure><img src="https://i.scdn.co/image/ab67616d0000b273dbe494d0433fb6ddd7387b81"> <h2>fishmonger</h2> <h3>underscores</h3> </figure> <p>fishmonger is underscores' very first album. Coming off of her years of songwriting and producing on soundcloud to mild success...</p>`
+        }
+
+        function zero() {
+            myContent.innerHTML = `<figure><img src="https://i.scdn.co/image/ab67616d0000b273ae859022b02dfe42b2c7a04d"> <h2>0</h2> <h3>Low Roar</h3> </figure> <p></p>`
+        }
+
+        function froge() {
+            myContent.innerHTML = `<figure><img src="https://i.scdn.co/image/ab67616d0000b273bcf683bad9ca8297e76f2408"> <h2>froge.mp3</h2> <h3>piri & tommy</h3> </figure> <p></p>`
+        }
+
+        function abnormal() {
+            myContent.innterHTML = `<figure><img src="https://media.pitchfork.com/photos/5e8ca8479a08f300084d48d7/1:1/w_800,h_800,c_limit/The%20New%20Abnormal_The%20Strokes.jpg"> <h2>The New Abnormal</h2> <h3>The Strokes</h3> </figure> <p></p>`
+        }
