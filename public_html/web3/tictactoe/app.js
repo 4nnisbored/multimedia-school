90fe1063db8b30fdc431be4da8290f207d980591
guyisbored <niod351@gmail.com>
2025-09-29 10:51:56 -0400

    aw yep

---
+++ public_html/web3/tictactoe/app.js
@@ -0,0 +1,78 @@
+
+let cases = document.querySelectorAll(".case");
+let replayBtn = document.querySelector("#replay");
+let paneauMessage = document.querySelector("#message");
+
+
+let joueurX = true;
+let gagnant = '';
+const patrons = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+
+let mes = [
+    "aaa",
+    "bbb",
+    "ccc"
+];
+
+for (let boite of cases) {
+    boite.active = true;
+        boite.addEventListener("click", function() {
+            if (boite.active){
+            if (joueurX) {
+                boite.innerText = "🪝";
+                joueurX = false;
+            }
+            else {
+                boite.innerText = "🐟";
+                joueurX = true;
+            }
+            boite.active = false;
+            valide()
+        }
+})
+}
+
+replayBtn.addEventListener("click", function () {
+    for (let boite of cases) {
+        boite.active = true;
+        boite.innerText = "";
+        joueurX = true;
+    }
+
+});
+
+const valide = function () {
+    for (let patron of patrons) {
+        let val1 = cases[patron[0]].innerText;
+        let val2 = cases[patron[1]].innerText;
+        let val3 = cases[patron[2]].innerText;
+        
+        if (val1 &&
+            val1 === val2 &&
+            val1 === val3) {
+                //console.log(getRandomInt(8));
+                window.alert(`${val1} gagnant`);
+
+                let m = mes[Math.floor(Math.random() * mes.length)]
+                console.log(m);
+
+                // function getRandomArbitrary(min, max) {
+                //  Math.random() * (max - min) + min; }
+                //boite.active = false;
+                for (let boite of cases) {
+                    boite.active = false;
+                }
+        }
+    }
+}
+
